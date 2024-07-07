392c2b72cb651ad4566672e162519ff434d854d3
Umar Hamzy <umar.hamzy@outlook.com>
2024-07-07 07:07:30 +0530

    Implement Hero section

---
+++ components/Grid.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      {/* Destructuring here so I don't have to type 'item' every time. i.e:- item.id, item.title etc. in BentoGridItem Props */}
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            imgClassName,
+            titleClassName,
+            img,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              key={id}
+              id={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
---
+++ components/ui/BentoGrid.tsx
@@ -0,0 +1,66 @@
+import { cn } from "@/utils/cn";
+
+export const BentoGrid = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoGridItem = ({
+  id,
+  className,
+  title,
+  description,
+  imgClassName,
+  titleClassName,
+  img,
+  spareImg,
+}: {
+  id?: number;
+  className?: string;
+  title?: string | React.ReactNode;
+  description?: string | React.ReactNode;
+  imgClassName?: string;
+  titleClassName?: string;
+  img?: string;
+  spareImg?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 ",
+        className
+      )}
+      style={{
+        //   add these two
+        //   you can generate the color from here https://cssgradient.io/
+        background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+      }}
+    >
+      <div className="group-hover/bento:translate-x-2 transition duration-200">
+        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+          {title}
+        </div>
+        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+          {description}
+        </div>
+      </div>
+    </div>
+  );
+};
